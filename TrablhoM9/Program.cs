bf49e2594bae6937d81122aedd6d2718e2f7f75d
Rafael Ferraz da Silva <a7660@esap.edu.pt>
2025-10-28 17:08:11 +0000

    Aula01-OperaçõesnoGitHub

---
+++ TrablhoM9/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrablhoM9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numeros = new int[10];
+            for (int i = 0; i < numeros.Length; i++)
+            {
+                Console.Write($"Digite um número inteiro {i+1}: ");
+                numeros[i] = int.Parse(Console.ReadLine());
+            }
+            foreach (int numero in numeros)
+            {
+                Console.WriteLine(numero);
+            }
+            Console.ReadKey();
+        }
+    }
+}
