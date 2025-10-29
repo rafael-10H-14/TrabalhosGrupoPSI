9e1e0c8ed033ad5a1a48ea266dfab6f7d6d68ee8
Rafael Ferraz da Silva <a7660@esap.edu.pt>
2025-10-29 11:40:36 +0000

    Aula02-Continuação da aula anterior

--- TrablhoM9/Program.cs
+++ TrablhoM9/Program.cs
@@ -16,10 +16,21 @@ namespace TrablhoM9
                 Console.Write($"Digite um número inteiro {i+1}: ");
                 numeros[i] = int.Parse(Console.ReadLine());
             }
+            int maximo = numeros[0];
+            int indice = 0;
+            for (int i = 1; i < numeros.Length; i++)
+            {
+                if (numeros[i] > maximo)
+                {
+                    maximo = numeros[i];
+                    indice = i;
+                }
+            }
             foreach (int numero in numeros)
             {
                 Console.WriteLine(numero);
             }
+            Console.WriteLine("Maior número: {0}, índice: {1}", maximo, indice);
             Console.ReadKey();
         }
     }
