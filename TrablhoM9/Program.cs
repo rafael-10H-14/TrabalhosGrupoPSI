a71e422df465fe48aee8af41c16fc3ac66084cef
Ana Carrelo <a7442@esap.edu.pt>
2025-10-29 12:40:42 +0000

    .

--- TrablhoM9/Program.cs
+++ TrablhoM9/Program.cs
@@ -32,7 +32,6 @@ namespace TrablhoM9
                 Console.WriteLine(numero);
             }
             Console.WriteLine("Maior número: {0}, índice: {1}", maximo, indice);
-            Console.WriteLine("Boa tarde");
             Console.ReadKey();
         }
     }
